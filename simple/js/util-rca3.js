7f75da75b7d2ee9c8771d2f8f5be1f7c73c5c9e9
Will Ackerly <willackerly@gmail.com>
2020-04-19 14:43:26 -0400

    frag to localStorage
    eom

--- simple/js/util-rca3.js
+++ simple/js/util-rca3.js
@@ -22,6 +22,15 @@
 
 /* eslint-disable no-unused-vars */
 
+
+//Function to consistently get URL into the localStorage
+if(window.location.hash) {
+  localStorage.setItem('pageFragment', window.location.hash.substr(1))
+} else {
+  //no new fragment, just use the local storage thing....
+}
+
+
 const BASE_URL = new RegExp(/^.*\//).exec(window.location.href);
 // const getById = (id) => document.getElementById(id);
 // const getQueryParam = (id) => new URL(window.location.href).searchParams.get(id);
